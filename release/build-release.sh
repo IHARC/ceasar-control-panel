e78678be625ad3bffa531667388c6fcc584e5765
jordan windows <jordan@iharc.ca>
2026-09-17 09:54:52 -0400

    Release Ceasar 1.0.17

--- release/build-release.sh
+++ release/build-release.sh
@@ -2,11 +2,11 @@
 
 set -euo pipefail
 
-version='1.0.16'
+version='1.0.17'
 commit=''
 deb_dir=''
 output_dir=''
-release_base_url='https://github.com/iharc-jordan/ceasar-control-panel/releases/download/v1.0.16'
+release_base_url='https://github.com/iharc-jordan/ceasar-control-panel/releases/download/v1.0.17'
 
 while [ "$#" -gt 0 ]; do
 	case "$1" in
