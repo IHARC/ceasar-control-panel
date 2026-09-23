72dfe00de90f5bf3a7f1beb0b1435c723cb2d3ba
jordan windows <jordan@iharc.ca>
2026-09-22 20:18:06 -0400

    Use installation accent for customer fallback

--- release/build-release.sh
+++ release/build-release.sh
@@ -2,11 +2,11 @@
 
 set -euo pipefail
 
-version='1.0.34'
+version='1.0.35'
 commit=''
 deb_dir=''
 output_dir=''
-release_base_url='https://github.com/IHARC/ceasar-control-panel/releases/download/v1.0.34'
+release_base_url='https://github.com/IHARC/ceasar-control-panel/releases/download/v1.0.35'
 
 while [ "$#" -gt 0 ]; do
 	case "$1" in
